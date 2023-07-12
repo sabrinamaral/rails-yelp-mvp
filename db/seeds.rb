0c9232da78a4638b2f592edc34b39b259139505c
sabrina amaral <sabrinamaraldev@gmail.com>
2023-07-11 22:27:55 -0300

    restaurant seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Faker::Config.locale = 'pt-BR'
+Restaurant.destroy_all
+puts 'Deleting old restaurants...'
+puts 'Creating new ones...'
+5.times do
+  p name = Faker::Restaurant.name
+  p address = Faker::Address.full_address
+  p phone_number = Faker::PhoneNumber.cell_phone
+  p category = Restaurant::CATEGORIES.sample
+  restaurant = Restaurant.new(name: name, address: address, phone_number: phone_number, category: category)
+  p restaurant.save
+end
