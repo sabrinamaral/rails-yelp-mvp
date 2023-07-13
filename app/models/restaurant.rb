5d41ad1197940fa6730d43f5c12a88577f282739
sabrina amaral <sabrinamaraldev@gmail.com>
2023-07-13 20:26:33 -0300

    refactoring reviews new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,9 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = %w[Chinese Italian Japanese French Belgian].freeze
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: CATEGORIES,
                                     message: `%{value} is not a valid category` }
-  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
