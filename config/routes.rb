a4e1ea2f68b2af1db5f07d66cf0fdc2bec3cf476
sabrina amaral <sabrinamaraldev@gmail.com>
2023-09-26 22:06:46 -0300

    change the restaurant route by the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: %i[index new create show] do
+  root to: 'restaurants#index'
+  resources :restaurants, only: %i[new create show] do
     resources :reviews, only: %i[create]
   end
 end
